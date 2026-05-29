bca78673a45a935b647c34c920584c42cbc4ba1b
Maxime Moreillon <m.moreillon@gmail.com>
2026-05-29 09:28:43 +0900

    now just passing through all request headers

--- example.ts
+++ example.ts
@@ -1,16 +1,16 @@
-import middleware from "./index"
-import Express from "express"
+import middleware from "./index";
+import express from "express";
 
-const app = Express()
-const EXPRESS_PORT = 7071
-const options = { url: `https://api.users.maximemoreillon.com/users/self` }
+const app = express();
+const EXPRESS_PORT = 7071;
+const options = { url: `http://10.115.1.100:30097/v3/users/self` };
 
-app.use(middleware(options))
+app.use(middleware(options));
 
 app.get("/", (req, res) => {
-  res.send("If you see this, you are authenticated")
-})
+  res.send(res.locals.user);
+});
 
 app.listen(EXPRESS_PORT, () => {
-  console.log(`[Express] Listening on port ${EXPRESS_PORT}`)
-})
+  console.log(`[Express] Listening on port ${EXPRESS_PORT}`);
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import Cookies from "cookies";
 import { Request, Response, NextFunction } from "express";
 
 interface ReqWithUser extends Request {
@@ -7,52 +6,21 @@ interface ReqWithUser extends Request {
   jwt?: string;
 }
 
-const retrieve_jwt = (req: Request, res: Response) =>
-  new Promise((resolve, reject) => {
-    // Retrieving the token from either cookies or authorization header
-    // NOTE: Did not need to be a promise
-
-    let { query, headers } = req;
-
-    let jwt;
-
-    // See if jwt available from authorization header
-    if (!jwt && headers?.authorization) {
-      jwt = headers?.authorization.split(" ")[1];
-    }
-
-    // Try to get JWT from cookies
-    if (!jwt) {
-      const cookies = new Cookies(req, res);
-      jwt = cookies.get("jwt") || cookies.get("token");
-    }
-
-    // Try to get the JWT from query parameters
-    if (!jwt) {
-      jwt = query.jwt || query.token;
-    }
-
-    if (!jwt) return reject(`Missing JWT`);
-
-    resolve(jwt);
-  });
-
 // NOTE: Middleware itself cannot be async
+// REALLY?
 const middleware =
   (options: any = {}) =>
-  (req: ReqWithUser, res: Response, next: NextFunction) => {
-    retrieve_jwt(req, res)
-      .then((jwt) => {
-        const { url } = options;
-        if (!url) throw "URL not provided";
+  async (req: ReqWithUser, res: Response, next: NextFunction) => {
+    const { url } = options;
+    if (!url) throw "URL not provided";
 
-        res.locals.jwt = jwt;
-        req.jwt = jwt as string;
+    // NOTE: previous version was pasing JWT to handler
+    // res.locals.jwt = jwt;
+    // req.jwt = jwt as string;
 
-        const headers = { Authorization: `Bearer ${jwt}` };
+    return axios
+      .get(url, { headers: req.headers })
 
-        return axios.get(url, { headers });
-      })
       .then(({ data }) => {
         res.locals.user = data;
         req.user = data;
